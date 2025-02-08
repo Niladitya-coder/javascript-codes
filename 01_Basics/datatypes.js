fe193d3f8dbc3724cd1df407a258d882cd1e508c
NILADITYA KAR <niladityakar.it2022@nsec.ac.in>
2025-02-08 15:42:22 +0000

    added datatypes.js

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,17 @@
+//DataTypes in JS
+//PRIMITIVES
+let name="Niladitya"; //string
+let age=21; //number
+//bigint
+let isAdult=true; //boolean
+let city; //undefined
+//null=> datatype and also a standalone value
+//symbol
+console.table([name,age,isAdult,city])
+console.log(typeof name); //Prints the datatype of name variable
+console.log(typeof undefined); //Output will be undefined
+console.log(typeof null); //Output will be object
+
+
+//NON-PRIMITIVES
+//Objects
