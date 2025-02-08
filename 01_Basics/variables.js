330df05e12ef118cb5f1c3bd7b56d8471ccfe7fd
NILADITYA KAR <niladityakar.it2022@nsec.ac.in>
2025-02-08 15:18:07 +0000

    added variables.js

---
+++ 01_Basics/variables.js
@@ -0,0 +1,8 @@
+const accountId=1234;
+let accountName="Niladitya";
+var accountEmail="niladitya@gmail.com"; //We should not use var
+accountPassword="Nila1234";
+let accountCity;
+accountName="Kar";
+console.log(accountName);
+console.table([accountId,accountName,accountEmail,accountPassword,accountCity]);
